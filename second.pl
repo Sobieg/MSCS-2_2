f9d7f4c2c49754479d1278d5d69f7f3756d41cb2
Sobieg <sobiegrus@gmail.com>
2019-02-27 22:06:16 +0300

    Create second.pl
    init

---
+++ second.pl
@@ -0,0 +1,46 @@
+:-dynamic subject/1.
+:-dynamic object/1.
+
+/*субъекты -- если остаток от деления на 3 у числа равен 1*/
+
+object(O):-subject(O).
+
+object(o2).
+object(o3).
+object(o5).
+object(o6).
+object(o8).
+
+subject(s1).
+subject(s4).
+subject(s7).
+
+right(r).
+right(w).
+
+
+
+print_matrix:-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
